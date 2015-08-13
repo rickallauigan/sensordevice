a7ecc00d696ed992e45b2c119ffcb8e3613b55cc
allauigan.rick@gmail.com <allauigan.rick@gmail.com>
2015-08-13 13:43:59 +0800

    SD + IR

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# sensordevice
-# v
+# Sensor Device
--- sdcodes.ino
+++ sdcodes.ino
@@ -1,116 +1,78 @@
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(2, 3);                                                      //your pins to serial communication
-int valor;
-String token = "gsm";      //your token to post value
-#define trigPin A1
-#define echoPin A0
+#include <SD.h>
+SoftwareSerial gprsSerial(2, 3);
 
+File sdLog;
+void d(int dly){delay(dly);ShowSerialData();}
 void setup()
 {
-  mySerial.begin(19200);                                                            //the GPRS baud rate
-  Serial.begin(19200);    
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-//  delay(1000);
+  gprsSerial.begin(19200);
+  Serial.begin(19200);
+  Serial.print("Loading SDCARD_");
+  pinMode(10, OUTPUT);
+  if (!SD.begin(10)) {
+    Serial.println("SD FAILED");
+    return;
+  }
+  Serial.println("SD OK");
+  
+  d(1000);
 }
+
 void loop()
 {
-  long duration, distance;
-  digitalWrite(trigPin, LOW);  // Added this line
-  delayMicroseconds(2); // Added this line
-  digitalWrite(trigPin, HIGH);
-//  delayMicroseconds(1000); - Removed this line
-  delayMicroseconds(10); // Added this line
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration/2) / 25.54;
-  if (distance >= 200 || distance <= 0){
-    Serial.println("Out of range");
-  }
-  else {
-    Serial.print(distance);
-    Serial.println(" cm");
-  }
-  delay(500);
-  
-    //int value = analogRead(A0);                                                     //read pin A0 from your arduino
-    save_value(String(distance));                                                      //call the save_value function
-    if (mySerial.available())
-    Serial.write(mySerial.read());
+    int IR = pow(3027.4/analogRead(A0), 1.2134);
+    Cache(String(IR));
+    Send(String(IR));
+    if (gprsSerial.available())
+    Serial.write(gprsSerial.read());
+    
 }
-//this function is to send the sensor data to Ubidots, you should see the new value in Ubidots after executing this function
-void save_value(String value)
+
+void Cache(String data){
+  sdLog = SD.open("DATA.txt", FILE_WRITE);
+  if (sdLog) {
+    Serial.print("Writing to DATA.txt...");
+    sdLog.println("TEST STRING");
+    sdLog.close();
+    Serial.println("done.");
+  } else {
+    Serial.println("DATA.txt ERR!");
+  }
+}
+void Send(String value)
 {
   int num;
   String le;
   String var;
-  var="{\"value\"unsure emoticon""+ value + "\"}";
+  var="{\"value\":\""+ value + "\"}";
   num=var.length();
   le=String(num);
-  for(int i = 0;i<1;i++)
-  {
-    mySerial.println("AT+CGATT?");                                                   //this is made repeatedly because it is unstable
+  for(int i = 0;i<1;i++){
+    gprsSerial.println("AT+CGATT?");
     delay(2000);
     ShowSerialData();
   }
-  mySerial.println("AT+CSTT=\"web.vmc.net.co\"");                                    //replace with your providers' APN
-  delay(1000);
-  ShowSerialData();
-  mySerial.println("AT+CIICR");                                                      //bring up wireless connection
-  delay(3000);
-  ShowSerialData();
-  mySerial.println("AT+CIFSR");                                                      //get local IP adress
-  delay(2000);
-  ShowSerialData();
-  mySerial.println("AT+CIPSPRT=0");
-  delay(3000);
-  ShowSerialData();
-  mySerial.println("AT+CIPSTART=\"tcp\",\"bukidutility.appspot.com\",\"80\"");             //start up the connection
-  delay(3000);
-  ShowSerialData();
-  mySerial.println("AT+CIPSEND");                                                    //begin send data to remote server
-  delay(3000);
-  ShowSerialData();
-  mySerial.print("POST /api/v1/farm/001");
-  delay(100);
-  ShowSerialData();
-  mySerial.println(" HTTP/1.1");
-  delay(100);
-  ShowSerialData();
-  mySerial.println("Content-Type: application/json");
-  delay(100);
-  ShowSerialData();
-  mySerial.println("Content-Length: "+le);
-  delay(100);
-  ShowSerialData();
-  mySerial.print("X-Farm-Token: ");
-  delay(100);
-  ShowSerialData();
-  mySerial.println(token);
-  delay(100);
-  ShowSerialData();
-  mySerial.println("Host: bukidutility.appspot.com");
-  delay(100);
-  ShowSerialData();
-  mySerial.println();
-  delay(100);
-  ShowSerialData();
-  mySerial.println(var);
-  delay(100);
-  ShowSerialData();
-  mySerial.println();
-  delay(100);
-  ShowSerialData();
-  mySerial.println((char)26);
-  delay(7000);
-  mySerial.println();
-  ShowSerialData();
-  mySerial.println("AT+CIPCLOSE");                                                //close the communication
-  delay(1000);
-  ShowSerialData();
+  ("AT+CSTT=\"web.vmc.net.co\"");d(1000);
+  gprsSerial.println("AT+CIPSTART=\"tcp\",\"Xinner-suprstate-820.appspot.com\",\"80\"");d(3000);
+  gprsSerial.println("AT+CIPSEND");d(3000);
+  gprsSerial.print("POST /api/v1/farm/1001/variable/0001");d(100);
+  gprsSerial.println(" HTTP/1.1");d(100);
+  gprsSerial.println("Content-Type: application/json");d(100);
+  gprsSerial.println("Content-Length: "+le);d(100);
+  gprsSerial.print("X-Farm-Token: ");d(100);
+  gprsSerial.println("\"token\"");d(100);
+  gprsSerial.println("Host: Xinner-suprstate-820.appspot.com");d(100);
+  gprsSerial.println();d(100);
+  gprsSerial.println(var);d(100);
+  gprsSerial.println();d(100);
+  gprsSerial.println((char)26);delay(5000);
+  gprsSerial.println();ShowSerialData();
+  gprsSerial.println("AT+CIPCLOSE");d(1000);
 }
-void ShowSerialData()
-{
-  while(mySerial.available()!=0)
-  Serial.write(mySerial.read());
+void ShowSerialData(){
+  while(gprsSerial.available()!=0)
+  Serial.write(gprsSerial.read());
 }
+
+
